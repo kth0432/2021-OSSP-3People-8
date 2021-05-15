4c76506b5cf06e0e51d4f3c73b809917c5663a98
Kim Tae Hwan <65579085+kth0432@users.noreply.github.com>
2021-05-15 16:36:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import sys
 import mode_one
 import mode_two
+import time
 
 user_size = 200     # 사용자 지정 화면 크기
 level_size = 2      # 값이 커지면 이미지가 작게 보임
@@ -21,6 +22,7 @@ if __name__ == '__main__':
 
         user_size = round(scr_size / level_size)
         sprites.get_size(user_size, level_size)
+        time.sleep(0.1)
 
         if level_size == 1 :
             scr_size, level_size = mode_one.main(scr_size, level_size)
